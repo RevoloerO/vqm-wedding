7709afdf732ea595b222f3f7e2acec3dda014c24
Vuong Quyen Mai <59490873+RevoloerO@users.noreply.github.com>
2025-02-10 00:37:14 -0500

    v0.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
-import { useState } from 'react'
 import NavBar from './NavBar.jsx';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const Banner = () => {
-    return (
-      <div className='banner'>
-        <h1>Quyen Mai & Hien Dang</h1>
-      </div>
-    )
-  }
   return (
     <div>
-      <NavBar />
-      <Banner />
+      
     </div>
   )
 }
